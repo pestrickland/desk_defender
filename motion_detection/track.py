a548207142e14a2c4b7b24d8b55722d11c745d2c
Paul Strickland <p.e.strickland@gmail.com>
2016-05-19 22:01:34 +0000

    Work on Camera class.

--- motion_detection/track.py
+++ motion_detection/track.py
@@ -4,11 +4,38 @@ import time
 import argparse
 import datetime
 import imutils
+from imutils.video import VideoStream
 import gdrive
 import tempimg
 import logging
 from picamera import PiCamera
 from picamera.array import PiRGBArray
+
+
+class Camera():
+	"""A camera object."""
+	
+	def __init__(self, args):
+		self.args = args
+		self.stopped = False
+
+	def start(self):
+
+		stream = VideoStream(usePiCamera=args["picamera"] > 0).start()
+		time.sleep(2)
+
+		while True:
+			# Grab a frame and resize it.
+			frame = stream.read()
+			frame = imutils.resize(frame, width=400)
+
+			if self.stopped == True:
+				break
+
+			
+
+			if key == ord("q"):
+				self.stopped = True	
 
 
 class Tracker():
@@ -24,11 +51,6 @@ class Tracker():
         camera.resolution = self.resolution
         camera.framerate = self.framerate
         self.camera = camera
-
-    def capture(self):
-        self.raw_capture = PiRGBArray(self.camera, size=self.resolution)
-        logging.info("Warming up")
-        time.sleep(1.5)
 
         last_uploaded = datetime.datetime.now()
         motion_counter = 0
@@ -116,98 +138,32 @@ def main():
                         action="store_true")
     args = parser.parse_args()
 
-    try:
-        from picamera import PiCamera
-        from picamera.array import PiRGBArray
-        camera = PiCamera()
-        camera.resolution = (640, 480)
+    camera = Camera(args)
 
-    except ImportError:
-        camera = cv2.VideoCapture(0)
 
-    camera.framerate = 15
-    rawCapture = PiRGBArray(camera, size=(640, 480))
-    avg = None
-    print("[INFO] warming up...")
-    time.sleep(2.5)
-    avg = None
-    lastUploaded = datetime.datetime.now()
-    motionCounter = 0
+    if args.upload:
+        if text == "Occupied":
+            if (timestamp - lastUploaded).seconds >= 3:
+                motionCounter += 1
 
-    for f in camera.capture_continuous(rawCapture, format="bgr",
-                                       use_video_port=True):
-        frame = f.array
-        timestamp = datetime.datetime.now()
-        text = "Unoccupied"
-        frame = imutils.resize(frame, width=500)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (21, 21), 0)
-        if avg is None:
-            print("[INFO] starting background model...")
-            avg = gray.copy().astype("float")
-            rawCapture.truncate(0)
-            continue
-        cv2.accumulateWeighted(gray, avg, 0.5)
-        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-        # threshold the delta image, dilate the thresholded image to fill
-        # in holes, then find contours on thresholded image
-        thresh = cv2.threshold(frameDelta, 5, 255,
-                               cv2.THRESH_BINARY)[1]
-        thresh = cv2.dilate(thresh, None, iterations=2)
-        (dm, contours, dm) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                              cv2.CHAIN_APPROX_SIMPLE)
+                if motionCounter >= 8:
+                    t = tempimg.TempImage()
+                    cv2.imwrite(t.path, frame)
 
-        # Loop over each contour.
-        for c in contours:
-            # If the contour is too small, ignore it.
-            if cv2.contourArea(c) < 5000:
-                continue
+                    print("[UPLOAD] {}".format(ts))
+                    file_name = "PiCam {}".format(ts)
+                    gdrive.upload_to_drive(t.path, file_name)
+                    t.cleanup()
 
-            # Compute the bounding box for the contour, draw it on the frame,
-            # and update the text.
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            text = "Occupied"
+                    lastUploaded = timestamp
+                    motionCounter = 0
 
-            moments = cv2.moments(c)
-            cx = int(moments["m10"] / moments["m00"])
-            cy = int(moments["m01"] / moments["m00"])
-
-            # Draw circle on contour centroid.
-            cv2.circle(frame, (cx, cy), 10, (0, 0, 255))
-
-            print("Coordinates: {}, {}".format(cx, cy))
-
-        # draw the text and timestamp on the frame
-        ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-        cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        cv2.putText(frame, ts, (10, frame.shape[0] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
-        if args.upload:
-            if text == "Occupied":
-                if (timestamp - lastUploaded).seconds >= 3:
-                    motionCounter += 1
-
-                    if motionCounter >= 8:
-                        t = tempimg.TempImage()
-                        cv2.imwrite(t.path, frame)
-
-                        print("[UPLOAD] {}".format(ts))
-                        file_name = "PiCam {}".format(ts)
-                        gdrive.upload_to_drive(t.path, file_name)
-                        t.cleanup()
-
-                        lastUploaded = timestamp
-                        motionCounter = 0
-
-        if args.live:
-            cv2.imshow("Feed", frame)
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord("q"):
-                break
-            rawCapture.truncate(0)
+    if args.live:
+        cv2.imshow("Feed", frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            camera.stop()
+        rawCapture.truncate(0)
 
 if __name__ == '__main__':
     main()
